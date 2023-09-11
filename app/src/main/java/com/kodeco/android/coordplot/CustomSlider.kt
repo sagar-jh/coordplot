4d3bfa4be9f3f38c04acceb9117264b5a33dd65d
sagar-jh <sagar@jhtechnologies.com>
2023-09-11 10:42:57 -0700

    Modified to fix the colors for sliders

--- app/src/main/java/com/kodeco/android/coordplot/CustomSlider.kt
+++ app/src/main/java/com/kodeco/android/coordplot/CustomSlider.kt
@@ -2,15 +2,16 @@ package com.kodeco.android.coordplot
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.Slider
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import kotlin.math.roundToInt
 
 @Composable
-fun CustomSlider(modifier: Modifier = Modifier, value : Float =0.5f, changeValue : (Float) -> Unit,text : String){
+fun CustomSlider(modifier: Modifier = Modifier, value : Float =0.5f, changeValue : (Float) -> Unit={},text : String ="Axis"){
     Text(text = "${text}: ${(value*100).roundToInt()}",
         Modifier
             .padding(8.dp)
@@ -20,5 +21,9 @@ fun CustomSlider(modifier: Modifier = Modifier, value : Float =0.5f, changeValue
         valueRange = 0.01f..1f,
         onValueChange = changeValue
 
+
     )
 }
+
+
+
--- app/src/main/java/com/kodeco/android/coordplot/ui/theme/Theme.kt
+++ app/src/main/java/com/kodeco/android/coordplot/ui/theme/Theme.kt
@@ -2,6 +2,7 @@ package com.kodeco.android.coordplot.ui.theme
 
 import android.app.Activity
 import android.os.Build
+import android.util.Log
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
@@ -55,12 +56,14 @@ fun MyApplicationTheme(
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
+
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
             window.statusBarColor = colorScheme.primary.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+
         }
     }
 
