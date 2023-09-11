7ce54b1a2a5042118f868ab13a8c702ef1f4efe8
sagar-jh <sagar@jhtechnologies.com>
2023-09-10 19:59:16 -0700

    Changing the code to save changes on Orientation Change

--- app/src/main/java/com/kodeco/android/coordplot/MainActivity.kt
+++ app/src/main/java/com/kodeco/android/coordplot/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -41,8 +42,8 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun PlotSurface() {
-    var xPercent: Float by remember { mutableStateOf(0.5f) }
-    var yPercent: Float by remember { mutableStateOf(0.5f) }
+    var xPercent: Float by rememberSaveable { mutableStateOf(0.5f) }
+    var yPercent: Float by rememberSaveable { mutableStateOf(0.5f) }
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = Color.White
